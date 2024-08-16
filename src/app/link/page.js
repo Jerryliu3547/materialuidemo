e6027d0a666fcc394623911a6188ef0170e42c13
Dianze Liu <114109108+Jerryliu3547@users.noreply.github.com>
2024-08-16 04:54:58 +0000

    add link

---
+++ src/app/link/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Stack, Link } from '@mui/material'
+
+const page = () => {
+  return (
+    <Stack spacing={2} direction='row'>
+        <Link href='#' variant='body2'>Link</Link>
+
+        <Link href='#' variant='h6' underline='none' color='salmon'>
+            seconday no underline variant 6
+        </Link>
+    </Stack>
+  )
+}
+
+export default page
