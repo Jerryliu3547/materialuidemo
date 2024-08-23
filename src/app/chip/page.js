2169c5731de5b03c72b836519fe3833bd4346c74
Jerryliu3547 <liudianze1994@gmail.com>
2024-08-22 19:33:55 -0700

    add chip

--- src/app/chip/page.js
+++ src/app/chip/page.js
@@ -1,1 +1,38 @@
-ra
+'use client'
+import React from 'react'
+import {Stack, Chip, Typography, Avatar} from '@mui/material'
+import FaceIcon from '@mui/icons-material/Face'
+import {useState} from 'react'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+
+const page = () => {
+  const [chips, setChips] = useState(['chips1', 'chips2', 'chips3'])  
+  const handleDelete = (chipToDelete) =>{
+    setChips(chips => chips.filter(item =>item!==chipToDelete))
+  }
+  return (
+    <div className='bg-slate-100 p-5'>
+        <Stack direction='row' spacing={1}>
+            <Chip label='chip 1' color='primary' />
+            <Chip label='chip 2' color='secondary' />
+            <Chip label='chip 3' />
+            <Chip label='with Avatars' color='success'  avatar={<Avatar>DL</Avatar>}/>
+            <Chip label='with Icon' color= 'secondary' icon={<FaceIcon/>}/>
+    
+        </Stack>
+        <Typography variant='h6' gutterBottom className='text-black'>On delete Case</Typography>
+        <Stack direction='row' spacing={1}>
+            {chips.map((chip, index)=>{
+                return (
+                    <Chip key={index} label={chip} color='primary' onDelete={()=> handleDelete(chip)} icon={<DeleteForeverIcon/> }/>
+                )
+            })}
+
+
+        </Stack>
+    </div>
+  )
+}
+
+export default page
