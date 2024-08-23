d51ec418828426168f61531fc3c558235540a4e8
Jerryliu3547 <liudianze1994@gmail.com>
2024-08-22 21:09:55 -0700

    add table to demo

--- src/app/table/page.js
+++ src/app/table/page.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import {TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material'
 
 const page = () => {
   return (
-    <div>p
+    <div className='p-10'>
+      <TableContainer component={Paper} sx={{maxHeight:'200px'}}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Age</TableCell>
+              <TableCell>Country</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>John Doe</TableCell>
+              <TableCell>25</TableCell>
+              <TableCell>USA</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Jane Doe</TableCell>
+              <TableCell>30</TableCell>
+              <TableCell>Canada</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>John Doe</TableCell>
+              <TableCell>25</TableCell>
+              <TableCell>USA</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Jane Doe</TableCell>
+              <TableCell>30</TableCell>
+              <TableCell>Canada</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Jane Doe</TableCell>
+              <TableCell>30</TableCell>
+              <TableCell>Canada</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Jane Doe</TableCell>
+              <TableCell>30</TableCell>
+              <TableCell>Canada</TableCell>
+            </TableRow>
+            {/* Add more rows as needed */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
         
     </div>
   )
